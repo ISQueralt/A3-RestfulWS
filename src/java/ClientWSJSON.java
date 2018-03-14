b0b6a93a1309dfde8a58ca22f929c0682d49a4c6
quera <quera@LAPTOP-BDR5N6KR>
2018-03-14 17:56:32 +0000

    Using Gson

--- src/java/ClientWSJSON.java
+++ src/java/ClientWSJSON.java
@@ -1,3 +1,4 @@
+import com.google.gson.Gson;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,6 +6,7 @@ import java.net.URL;
 import java.net.URLConnection;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 
 /**
  *
@@ -23,19 +25,17 @@ public class ClientWSJSON {
             String line;
             while ((line = in.readLine()) != null){
                 rest+=line;
-            }   System.out.println("Respuesta del servidor: " + rest);
+            }   
         }
         
         JSONObject obj = new JSONObject(rest);
-        System.out.println("Object: " + obj);
         JSONObject res = obj.getJSONObject("RestResponse").getJSONObject("result");
-        System.out.println("res: " + res);
         
-        String name = res.get("name").toString();
-        String alpha2 = res.get("alpha2_code").toString();
-        String alpha3 = res.get("alpha3_code").toString();
-        
-        Country country = new Country(name, alpha2, alpha3); 
-        System.out.println("Country = " + country.toString());
+        Gson gson = new Gson();
+        Country country = (Country) gson.fromJson(res.toString(), Country.class); 
+       
+        System.out.println("Name = " + country.getName());
+        System.out.println("Aplha2_code = " + country.getAlpha2_code());
+        System.out.println("Alpha3_code = " + country.getAlpha3_code());
     }
 }
